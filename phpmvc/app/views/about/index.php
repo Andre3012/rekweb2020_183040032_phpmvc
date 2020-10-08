95b6a9e8791feb05064d9441d07e0cc566ab783e
andrekristiantoro <71427246+Andre3012@users.noreply.github.com>
2020-10-08 09:49:58 +0700

    Add files via upload

---
+++ phpmvc/app/views/about/index.php
@@ -0,0 +1,7 @@
+<div class="container">
+
+	<h1 class="mt-4">About Me</h1>
+	<img src="<?= BASEURL; ?>/img/profile.jpeg" alt="Andre Kristiantoro" width="300" class="rounded-box">
+	<p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+
+</div>
